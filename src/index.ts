3485cb5b6a756dda36835f83f60ac313bb8b50de
Claudio Pennati <claudio.pennati@gmail.com>
2023-01-20 10:02:21 +0100

    `createMysqlPdo` function exported to better typing mysqlOptions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Pdo } from 'lupdo';
 import MysqlDriver from './mysql-driver';
+import { MysqlOptions } from './types';
+
+import PdoAttributes from 'lupdo/dist/typings/types/pdo-attributes';
+import { PoolOptions } from 'lupdo/dist/typings/types/pdo-pool';
 Pdo.addDriver('mysql', MysqlDriver);
 Pdo.addDriver('mariadb', MysqlDriver);
+
+export function createMysqlPdo(options: MysqlOptions, poolOptions?: PoolOptions, attributes?: PdoAttributes): Pdo {
+    return new Pdo('mysql', options, poolOptions, attributes);
+}
 
 export { default as MysqlDriver } from './mysql-driver';
 export * from './types';
